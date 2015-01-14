828838cb01f7bdfbb03027fe4dd4a45baf6a18f2
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-01-14 12:08:05 -0600

    Initial Commit

---
+++ DieHard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DieHard
+//
+//  Created by Kyle Magnesen on 1/14/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
